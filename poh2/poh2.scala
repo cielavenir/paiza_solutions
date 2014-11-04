bc3700f581f8bb71a8950712bddfe8a6b9c2cf68
cielavenir <cielartisan@gmail.com>
2014-11-05 00:26:05 +0900

    modify scala

--- poh2/poh2.scala
+++ poh2/poh2.scala
@@ -1,39 +1,42 @@
+//usr/bin/env scala $0 $@;exit
 import scala.util.control.Breaks
-object Main extends App{
-	val Array(h,w) = readLine().split(" ").map(_.toInt)
-	val a=Array.ofDim[Int](h,w,h)
-	val accum=Array.ofDim[Int](h,w+1)
-	for(i<-0 to h-1){
-		val line=readLine();
-		for(j<-0 to w-1)a(i)(j)(0)=(line.charAt(j)-'0')^1
-	}
-	for(i<-0 to h-1){
-		accum(0)(a(i)(0)(0))+=1
-		for(j<-1 to w-1){
-			if(a(i)(j)(0)>0){
-				a(i)(j)(0)+=a(i)(j-1)(0)
-				accum(0)(a(i)(j)(0))+=1
-			}
+object Main{
+	def main(args:Array[String]){
+		val Array(h,w) = readLine().split(" ").map(_.toInt)
+		val a=Array.ofDim[Int](h,w,h)
+		val accum=Array.ofDim[Int](h,w+1)
+		for(i<-0 until h){
+			val line=readLine();
+			for(j<-0 until w)a(i)(j)(0)=(line.charAt(j)-'0')^1
 		}
-	}
-	val b=new Breaks
-	for(i<-1 to h-1){
-		for(j<-0 to w-1){
-			b.breakable{
-				for(k<-1 to i){
-					a(i)(j)(k)=Math.min(a(i-1)(j)(k-1),a(i)(j)(k-1))
-					if(a(i)(j)(k)==0)b.break
-					accum(k)(a(i)(j)(k))+=1
+		for(i<-0 until h){
+			accum(0)(a(i)(0)(0))+=1
+			for(j<-1 until w){
+				if(a(i)(j)(0)>0){
+					a(i)(j)(0)+=a(i)(j-1)(0)
+					accum(0)(a(i)(j)(0))+=1
 				}
 			}
 		}
-	}
-	for(i<-0 to h-1){
-		for(j<-w-1 to 0 by -1)accum(i)(j)+=accum(i)(j+1)
-	}
-	val K=readInt()
-	for(k<-1 to K){
-		val Array(s,t) = readLine().split(" ").map(_.toInt)
-		println(if(s<=h&&t<=w) accum(s-1)(t) else 0)
+		val b=new Breaks
+		for(i<-1 until h){
+			for(j<-0 until w){
+				b.breakable{
+					for(k<-1 to i){
+						a(i)(j)(k)=Math.min(a(i-1)(j)(k-1),a(i)(j)(k-1))
+						if(a(i)(j)(k)==0)b.break
+						accum(k)(a(i)(j)(k))+=1
+					}
+				}
+			}
+		}
+		for(i<-0 until h){
+			for(j<-w-1 to 0 by -1)accum(i)(j)+=accum(i)(j+1)
+		}
+		val K=readInt()
+		for(k<-1 to K){
+			val Array(s,t) = readLine().split(" ").map(_.toInt)
+			println(if(s<=h&&t<=w) accum(s-1)(t) else 0)
+		}
 	}
 }
--- pohlite/pohlite.scala
+++ pohlite/pohlite.scala
@@ -1,30 +1,33 @@
+//usr/bin/env scala $0 $@;exit
 //must use spaces rather than tabs, or WA somehow.
-object Main extends App{
-    val M=readInt()
-    val N=readInt()
-    val num=new Array[Int](N)
-    val cost=new Array[Int](N)
-    var total=0
-    for(j<-0 to N-1){
-        val Array(q,r) = readLine().split(" ").map(_.toInt)
-        total+=q
-        num(j)=q
-        cost(j)=r
-    }
-    val bag=new Array[Int](total+1)
-    for(i<-1 to total)bag(i)=(-1)
-    for(j<-0 to N-1){
-        for(i<-total to num(j) by -1){
-            if(bag(i-num(j))>=0){
-                if(bag(i)<0||bag(i)>bag(i-num(j))+cost(j)){
-                    bag(i)=bag(i-num(j))+cost(j)
+object Main{
+    def main(args:Array[String]){
+        val M=readInt()
+        val N=readInt()
+        val num=new Array[Int](N)
+        val cost=new Array[Int](N)
+        var total=0
+        for(j<-0 until N){
+            val Array(q,r) = readLine().split(" ").map(_.toInt)
+            total+=q
+            num(j)=q
+            cost(j)=r
+        }
+        val bag=new Array[Int](total+1)
+        for(i<-1 to total)bag(i)=(-1)
+        for(j<-0 until N){
+            for(i<-total to num(j) by -1){
+                if(bag(i-num(j))>=0){
+                    if(bag(i)<0||bag(i)>bag(i-num(j))+cost(j)){
+                        bag(i)=bag(i-num(j))+cost(j)
+                    }
                 }
             }
         }
+        var j=(-1)
+        for(i<-M to total){
+            if(j<0||(bag(i)>=0&&j>bag(i)))j=bag(i)
+        }
+        println(j)
     }
-    var j=(-1)
-    for(i<-M to total){
-        if(j<0||(bag(i)>=0&&j>bag(i)))j=bag(i)
-    }
-    println(j)
 }
