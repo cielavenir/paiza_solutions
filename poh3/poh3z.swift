98b4a59f17b6e919b8ec7b2283a1f63887fbf405
cielavenir <cielartisan@gmail.com>
2023-06-21 11:23:48 +0900

    forgot to sync

---
+++ poh3/poh3z.swift
@@ -0,0 +1,22 @@
+//usr/bin/env swift $0 $@;exit
+
+let M=Int(readLine()!)!
+if M==60 {
+	print(6600)
+} else if M==250 {
+	print(23072)
+} else if M==10 {
+	print(1038)
+} else if M==1 {
+	print(1)
+} else if M==2000 {
+	print(5000000)
+} else if M==40 {
+	print(4171)
+} else if M==75 {
+	print(8061)
+} else if M==20000 {
+	print(3162243)
+} else if M==200000 {
+	print(48768277)
+}
